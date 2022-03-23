c504aa85fec26e4473dc500161f02ca6d0042b4f
rebecalasuenremon <98742881+rebecalasuenremon@users.noreply.github.com>
2022-03-23 09:12:55 +0100

    kjcdlsjahlfweafhmadeoiawedskzxyfeiwds

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 basic.forever(function () {
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
     } else {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-    }
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
-    } else {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        }
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        }
     }
 })
