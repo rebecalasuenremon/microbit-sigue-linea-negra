f98cc6f23a4b21c3d8acf4be20daeb83f6c390b3
rebecalasuenremon <98742881+rebecalasuenremon@users.noreply.github.com>
2022-03-23 09:00:19 +0100

    holiwis quuiero escribir en este teclado porque me encanta el tacto y la textura de este portatil

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
 basic.forever(function () {
-	
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    }
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    }
 })
